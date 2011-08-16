838e12691358150a34eb8d2bc18e311ccd7bffd2
shibakaneki <didier@sente.ch>
2011-08-16 11:40:09 +0200

    Added the icons in the project tree

--- ProjectTree.cpp
+++ ProjectTree.cpp
@@ -1,3 +1,6 @@
+#include <QDebug>
+#include <QFileInfo>
+
 #include "ProjectTree.h"
 #include "globalDefs.h"
 
@@ -9,4 +12,50 @@ ProjectTree::ProjectTree(QWidget *parent, const char *name):QTreeWidget(parent)
 ProjectTree::~ProjectTree()
 {
 
+}
+
+void ProjectTree::setProjectPath(const QString &path)
+{
+    // Clear the tree
+    this->clear();
+    QTreeWidgetItem* pRoot = this->invisibleRootItem();
+
+    // Get the current widget
+    QDir projectDir(path);
+    QFileInfo fi(path);
+
+    pItem = new QTreeWidgetItem(this);
+    pItem->setText(0, fi.baseName());
+    pItem->setIcon(0, QIcon(":resources/folder.png"));
+    pRoot->addChild(pItem);
+    getContent(&projectDir, pItem);
+}
+
+void ProjectTree::getContent(QDir *pParentDir, QTreeWidgetItem* pParentItem)
+{
+    if(NULL != pParentDir && NULL != pParentItem)
+    {
+        QStringList qslFiles = pParentDir->entryList();
+        for(int i = 0; i < qslFiles.size(); i++)
+        {
+            if(qslFiles.at(i) != "." && qslFiles.at(i) != "..")
+            {
+                pItem = new QTreeWidgetItem();
+                pItem->setText(0, qslFiles.at(i));
+                pParentItem->addChild(pItem);
+                QString itemPath = QString("%0/%1").arg(pParentDir->path()).arg(qslFiles.at(i));
+                QFileInfo fi(itemPath);
+                if(fi.isDir())
+                {
+                    pItem->setIcon(0, QIcon(":resources/folder.png"));
+                    QDir crntDir(itemPath);
+                    getContent(&crntDir, pItem);
+                }
+                else
+                {
+                    pItem->setIcon(0, QIcon(":resources/file.png"));
+                }
+            }
+        }
+    }
 }
