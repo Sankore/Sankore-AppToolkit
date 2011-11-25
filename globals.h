7d7312bef0e5c842e8275b601da47440608d40db
shibakaneki <didier@sente.ch>
2011-11-25 12:13:04 +0100

    Removed old files 	deleted:    AppWidget.h 	deleted:    DocumentTab.cpp 	deleted:    DocumentTab.h 	deleted:    DocumentWidget.cpp 	deleted:    DocumentWidget.h 	deleted:    HelpViewer.cpp 	deleted:    HelpViewer.h 	deleted:    LogWidget.cpp 	deleted:    LogWidget.h 	deleted:    MessageSelector.cpp 	deleted:    MessageSelector.h 	deleted:    ProjectTree.cpp 	deleted:    ProjectTree.h 	deleted:    SyntaxHighlighter.cpp 	deleted:    SyntaxHighlighter.h 	deleted:    TextDocument.cpp 	deleted:    TextDocument.h 	deleted:    UBWidgetUniboardAPI.cpp 	deleted:    UBWidgetUniboardAPI.h 	deleted:    WebDocument.cpp 	deleted:    WebDocument.h 	deleted:    WidgetManager.cpp 	deleted:    WidgetManager.h 	deleted:    globalDefs.h 	deleted:    globals.h 	deleted:    main.cpp 	deleted:    mainwindow.cpp 	deleted:    mainwindow.h 	deleted:    release.linux.sh 	deleted:    release.macx.sh

--- globals.h
+++
@@ -1,8 +0,0 @@
-#ifndef GLOBALS_H
-#define GLOBALS_H
-
-#define APP_NAME        "Sankore Application Toolkit"
-#define APP_ORG         "Open-Sankore.org"
-#define APP_VERSION     1.1
-
-#endif // GLOBALS_H
