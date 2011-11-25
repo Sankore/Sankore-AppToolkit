ba5a5200f1cf07d5357c032a46191d41afde9396
shibakaneki <didier@sente.ch>
2011-11-25 09:50:00 +0100

    Added some internal work

---
+++ globals.h
@@ -0,0 +1,8 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#define APP_NAME        "Sankore Application Toolkit"
+#define APP_ORG         "Open-Sankore.org"
+#define APP_VERSION     1.1
+
+#endif // GLOBALS_H
